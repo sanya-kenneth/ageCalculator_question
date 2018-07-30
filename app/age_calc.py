03517e07154b46e2437127b2e966bb0fa541205b
sanya kenneth <sanyakenneth@gmail.com>
2018-07-30 05:11:02 +0300

    Add final changes

--- app/age_calc.py
+++ app/age_calc.py
@@ -2,5 +2,8 @@ class Age_calc():
     def ageCalculator(self, birth_year):
         self.birth_year = birth_year
         current_year = 2018
-
-        return current_year - self.birth_year
+        year_type = (int)
+        if isinstance(birth_year,year_type):
+            return current_year - self.birth_year
+        else:
+            raise ValueError
--- tests/test_age_calc.py
+++ tests/test_age_calc.py
@@ -1,7 +1,16 @@
 import unittest
 from app.age_calc import Age_calc
 class Calc_tester(unittest.TestCase):
+    def setUP(self):
+        self.calculate = Age_calc()
+
     def test_correct_age_is_calculated(self):
            birth_year = 1990
-           calculate = Age_calc()
-           assert calculate.ageCalculator(birth_year) == 28
+           self.calculate = Age_calc()
+           calculated = self.calculate.ageCalculator(birth_year) 
+           self.assertEqual(28, calculated) 
+    
+    def test_year_is_input_as_integer(self):
+            self.calculate = Age_calc()
+            self.assertRaises(ValueError,self.calculate.ageCalculator,'nineteen ninety')
+        
